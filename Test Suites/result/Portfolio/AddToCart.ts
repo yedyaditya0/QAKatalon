093adc1add9791f5d3f463beaf44ea610b1314e2
yedyaditya0 <yedyaditya0@gmail.com>
2019-08-19 13:14:58 +0700

    Added AddToCart for Portfolio Reksadana

---
+++ Test Suites/result/Portfolio/AddToCart.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AddToCart</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d74028b6-ba16-4b71-93bf-f6ab42fae1c1</testSuiteGuid>
+   <testCaseLink>
+      <guid>45ce1df3-5572-45bf-9997-1c9e675db670</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LoginTrue</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ddd71e78-eb1b-4959-a035-ba428c0a7037</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OTP/LoginOTP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bb20fae2-7917-459b-b7d6-2c63b25dad16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Home</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9ab6a2a-b4ff-4ea3-8459-30459d82bc36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Portfolio/AddToCart/AddToCart</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
