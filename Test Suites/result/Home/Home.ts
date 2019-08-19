a088071213de7786406557c9161c9445d9d4f304
yedyaditya0 <yedyaditya0@gmail.com>
2019-08-19 17:32:21 +0700

    Added Reedem

---
+++ Test Suites/result/Home/Home.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Home</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>92b1e0cc-543b-4cf0-b9cd-8fdc841b41ed</testSuiteGuid>
+   <testCaseLink>
+      <guid>7599ae4b-67be-49b6-8e28-c03b810ea76a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LoginTrue</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>750d07d1-0a6f-40ca-9b73-036379b8929c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OTP/LoginOTP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86180dbd-3ab2-4c88-9533-0e026869783e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Home</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/result/Portfolio/Reedem.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Reedem</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3d04607a-1b14-42df-9f3f-62e40e542988</testSuiteGuid>
+   <testCaseLink>
+      <guid>90b427e7-b1c7-4c73-8642-b1b8b4e93a46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LoginTrue</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8e1257ed-6945-4462-85d9-2c1e62d28bc9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OTP/LoginOTP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>990858c0-35e8-4af4-96cf-8b4a148efb23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Home</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae82a96d-ca0a-4b64-ad06-b582c2acd78e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Portfolio/Reedem</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>12f86c6f-a481-4d53-9a69-ee958ac7695e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Portfolio/ReedemConfirmation</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
